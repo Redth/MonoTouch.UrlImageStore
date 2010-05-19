d12036945bf0df906f5bbd41affc92ff04d881ea
Jon <jondick@gmail.com>
2010-05-18 23:20:55 -0400

    initial commit of project

---
+++ Graphics.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.CoreFoundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.Dialog.UrlImageStore
+{
+	public static class Graphics
+	{
+		
+        // Child proof the image by rounding the edges of the image
+        public static UIImage RoundCorners (UIImage image)
+        {
+			if (image == null)
+				throw new ArgumentNullException ("image");
+			
+            UIGraphics.BeginImageContext (image.Size);
+			float imgWidth = image.Size.Width;
+			float imgHeight = image.Size.Height;
+
+            var c = UIGraphics.GetCurrentContext ();
+
+            c.BeginPath ();
+            c.MoveTo (imgWidth, imgHeight/2);
+            c.AddArcToPoint (imgWidth, imgHeight, imgWidth/2, imgHeight, 4);
+            c.AddArcToPoint (0, imgHeight, 0, imgHeight/2, 4);
+            c.AddArcToPoint (0, 0, imgWidth/2, 0, 4);
+            c.AddArcToPoint (imgWidth, 0, imgWidth, imgHeight/2, 4);
+            c.ClosePath ();
+            c.Clip ();
+
+            image.Draw (new PointF (0, 0));
+            var converted = UIGraphics.GetImageFromCurrentImageContext ();
+            UIGraphics.EndImageContext ();
+            return converted;
+        }
+		
+		public static UIImage Scale(UIImage image, float maxWidthAndHeight)
+		{
+			//Perform Image manipulation, make the image fit into a 48x48 tile without clipping.  
+			
+			float fWidth = image.Size.Width;
+			float fHeight = image.Size.Height;
+			float fTotal = fWidth>=fHeight?fWidth:fHeight;
+			float fDifPercent = maxWidthAndHeight / fTotal;
+			float fNewWidth = fWidth*fDifPercent;
+			float fNewHeight = fHeight*fDifPercent;
+			
+			SizeF newSize = new SizeF(fNewWidth,fNewHeight);
+			
+			UIGraphics.BeginImageContext (newSize);
+	        var context = UIGraphics.GetCurrentContext ();
+	        context.TranslateCTM (0, newSize.Height);
+	        context.ScaleCTM (1f, -1f);
+	
+	        context.DrawImage (new RectangleF (0, 0, newSize.Width, newSize.Height), image.CGImage);
+	
+	        var scaledImage = UIGraphics.GetImageFromCurrentImageContext();
+	        UIGraphics.EndImageContext();
+	
+			return scaledImage;
+		}
+	}
+}
+
---
+++ UrlImageStore.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using MonoTouch.CoreFoundation;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.Dialog.UrlImageStore
+{
+	public interface IUrlImageUpdated<TKey>
+	{
+		void UrlImageUpdated(TKey id);
+	}
+
+	public class UrlImageStore<TKey>
+	{
+		public delegate UIImage ProcessImageDelegate(UIImage img, TKey id);
+		
+		readonly static string baseDir = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), "..");
+		string picDir;
+		
+		const int maxWorkers = 4;
+		int threadCount = 0;
+
+		LRUCache<TKey, UIImage> cache;
+		Queue<UrlImageStoreRequest<TKey>> queue;
+		//NSString nsDispatcher = new NSString("x");
+	
+				
+		public UrlImageStore(int capacity, string storeName, ProcessImageDelegate processImage)
+		{
+			this.Capacity = capacity;
+			this.StoreName = storeName;
+			this.ProcessImage = processImage;
+			
+			cache = new LRUCache<TKey, UIImage>(capacity);
+			queue = new Queue<UrlImageStoreRequest<TKey>>();
+			
+			picDir = Path.Combine(baseDir, "Library/Caches/Pictures/" + storeName);
+			
+			if (!Directory.Exists("Library/Caches/Pictures/"))
+				Directory.CreateDirectory("Library/Caches/Pictures/");
+		}
+
+		public ProcessImageDelegate ProcessImage
+		{
+			get;
+			private set;
+		}
+		
+		public int Capacity
+		{
+			get;
+			private set;
+		}
+
+		public UIImage DefaultImage
+		{
+			get;
+			set;
+		}
+		
+		public string StoreName
+		{
+			get;
+			private set;	
+		}
+
+		public UIImage GetImage(TKey id)
+		{
+			UIImage result = this.DefaultImage;
+
+			lock (cache)
+			{
+				if (cache.ContainsKey(id))
+					result = cache[id];
+			}
+
+			return result;
+		}
+
+		public UIImage RequestImage(TKey id, string url, IUrlImageUpdated<TKey> notify)
+		{
+			//First see if the image is in memory cache already and return it if so
+			lock (cache)
+			{		
+				if (cache.ContainsKey(id))
+					return cache[id];
+			}
+			
+			//Next check for a saved file, and load it into cache and return it if found
+			string picFile = picDir + id + ".png";
+			if (File.Exists(picFile))
+			{
+				UIImage img = null;
+				
+				try { img = UIImage.FromFileUncached(picFile); }
+				catch { }
+				
+				if (img != null)
+				{					
+					AddToCache(id, img); //Add it to cache
+					return img; //Return this image
+				}
+			}
+
+			//At this point the file needs to be downloaded, so queue it up to download
+			lock (queue)
+			{
+				queue.Enqueue(new UrlImageStoreRequest<TKey>() { Id = id, Url = url, Notify = notify });
+			}
+
+			//If we haven't maxed out our threads we should start another to download the images
+			if (threadCount < maxWorkers)
+				ThreadPool.QueueUserWorkItem(new WaitCallback(DownloadImagesWorker));
+			
+			//Return the default while they wait for the queued download
+			return this.DefaultImage;
+		}
+
+		void DownloadImagesWorker(object state)
+		{
+			threadCount++;
+
+			UrlImageStoreRequest<TKey> nextReq = null;
+			
+			while ((nextReq = GetNextRequest()) != null)
+			{
+				UIImage img = null;
+
+				try { img = UIImage.LoadFromData(NSData.FromUrl(NSUrl.FromString(nextReq.Url))); }
+				catch { }
+				
+				if (img == null)
+					continue;
+
+				//See if the consumer needs to do any processing to the image
+				if (this.ProcessImage != null)
+					img = this.ProcessImage(img, nextReq.Id);
+					
+				//Add it to cache
+				AddToCache(nextReq.Id, img);
+
+				//Save it to disk
+				NSError err = null;
+				try { img.AsPNG().Save(picDir + nextReq.Id + ".png", false, out err); }
+				catch { }
+				
+				//Notify the listener waiting for this,
+				// but do this on the main thread so the user of this class doesn't worry about that
+				//nsDispatcher.BeginInvokeOnMainThread(delegate { nextReq.Notify.UrlImageUpdated(nextReq.Id); });
+				nextReq.Notify.UrlImageUpdated(nextReq.Id);
+			}
+
+			threadCount--;
+		}
+
+		void AddToCache(TKey id, UIImage img)
+		{
+			lock (cache)
+			{
+				if (cache.ContainsKey(id))
+					cache[id] = img;
+				else
+					cache.Add(id, img);
+			}
+		}
+		
+		UrlImageStoreRequest<TKey> GetNextRequest()
+		{
+			UrlImageStoreRequest<TKey> nextReq = null;
+
+			lock (queue)
+			{
+				if (queue.Count > 0)
+					nextReq = queue.Dequeue();
+			}
+
+			return nextReq;
+		}
+
+		public void ReclaimMemory()
+		{
+			lock (cache)
+			{
+				cache.ReclaimLRU(cache.Count / 4);
+			}
+		}
+	}
+
+	public class UrlImageStoreRequest<TKey>
+	{
+		public TKey Id
+		{
+			get;
+			set;
+		}
+
+		public string Url
+		{
+			get;
+			set;
+		}
+
+		public IUrlImageUpdated<TKey> Notify
+		{
+			get;
+			set;
+		}
+	}
+}
